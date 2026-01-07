f18ed338c3f4bf5b179cc5ab61ee093c4a6a373e
Will <yzj_000@163.com>
2026-01-07 23:15:07 +0800

    Add Spring Boot application main class

---
+++ src/main/java/io/will/springaiversionchaser/SpringAiVersionChaserApplication.java
@@ -0,0 +1,11 @@
+package io.will.springaiversionchaser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiVersionChaserApplication {
+    static void main(String[] args) {
+        SpringApplication.run(SpringAiVersionChaserApplication.class, args);
+    }
+}
